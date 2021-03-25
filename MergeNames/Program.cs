ae31330f31e66727aa6ac8f89507afb5c35d74f1
Geetanjali Singhal <geesingh2@publicisgroupe.net>
2021-03-25 23:09:24 +0530

    Add project files.

---
+++ MergeNames/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MergeNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+            string[] names2 = new string[] { "Olivia", "Sophia", "Emma"};
+            Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2)));
+        }
+    }
+}
---
+++ MergeNames/Unique.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MergeNames
+{
+    class MergeNames
+    {
+        public static string[] UniqueNames(string[] names1, string[] names2)
+        {
+            Dictionary<string, int> value = new Dictionary<string, int>();
+            string[] val = null;
+            int i = 0;
+            for (i = 0; i < names1.Length; i++)
+            {
+                value.Add(names1[i], i);
+            }
+            for (int j = 0; j < names2.Length; j++)
+            {
+                if (!value.ContainsKey(names2[j]))
+                {
+                    value.Add(names2[j], i);
+                }
+               
+            }
+            val = value.Select(z => z.Key).ToArray();
+            return val;
+        }
+    }
+}
